02195a990f64a98e2c7ecfea92c489117ba288f7
Charlie Dalton <charliedalton@oneanymousboi.hsd1.ca.comcast.net>
2018-07-19 13:40:48 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -27,6 +28,36 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+// GET /todos/31893819
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(todo) {
+      res.send({todo});
+    }
+    else {
+      res.status(404).send();
+    }
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+  // valid id using isValid
+    // 404 - send back empty send
+
+  // findById
+   // success case
+     // if todo - send it back
+     // if no todo - send back 404 with empty body
+   // error
+     // 400 - send back empty body back
 });
 
 app.listen(3000, () => {
