33fef2a5cc3c87249132660ca167c5230fa0ecb5
Charlie Dalton <charliedalton@oneanymousboi.hsd1.ca.comcast.net>
2018-07-20 09:44:31 -0700

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect('mongodb://charlesdalton:paris250@ds018498.mlab.com:18498/node-todo-api'
+  || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = { mongoose };
