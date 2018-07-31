fcdcab2c477f0046f4d68a631ec8d466f709b5fe
Charlie Dalton <charliedalton@oneanymousboi.hsd1.ca.comcast.net>
2018-07-31 10:56:06 -0700

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.get('/todos/:id', (req, res) => {
 
   Todo.findById(id).then((todo) => {
     if(todo) {
-      res.send({todo});
+      res.send({ todo });
     }
     else {
       res.status(404).send();
@@ -70,13 +70,13 @@ app.delete('/todos/:id', (req, res) => {
 
   Todo.findByIdAndRemove(id).then((todo) => {
     if(!todo) {
-      res.status(400).send();
+      res.status(404).send();
     }
     else {
-      res.status(200).send(todo);
+      res.status(200).send({todo});
     }
 
-    res.send(todo);
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
