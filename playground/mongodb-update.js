515a30240fa44d887521a74d62f083d17fa7b57e
Charlie Dalton <charliedalton@oneanymousboi.hsd1.ca.comcast.net>
2018-07-31 11:13:05 -0700

    Add PATCH /todos/:id

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -8,30 +8,30 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
 
-  // db.collection('Todos').findOneAndUpdate({
-  //   _id: new ObjectID("5b4f70cdb1210b7517e1e4be")
-  // }, {
-  //   $set: {
-  //     completed: true
-  //   }
-  // }, {
-  //   returnOriginal: false
-  // }).then((result) => {
-  //   console.log(result);
-  // });
+  db.collection('Todos').findOneAndUpdate({
+    _id: new ObjectID("5b4f70cdb1210b7517e1e4be")
+  }, {
+    $set: {
+      completed: true
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
 
-  // db.collection('Users').findOneAndUpdate({
-  //   _id: 123
-  // }, {
-  //   $set: { name: 'Charlie' },
-  //   $inc: { age: 1 }
-  // }, {
-  //   returnOriginal: false
-  // }).then((result) => {
-  //   console.log(result);
-  // }, (err) => {
-  //   console.log(`Error: ${err}`);
-  // });
+  db.collection('Users').findOneAndUpdate({
+    _id: 123
+  }, {
+    $set: { name: 'Charlie' },
+    $inc: { age: 1 }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  }, (err) => {
+    console.log(`Error: ${err}`);
+  });
 
   //client.close();
 });
